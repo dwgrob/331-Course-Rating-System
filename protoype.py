1b8661d2e5ca008709bcf2bc747726f8da1a94a6
Daniel Robinson <148491182+dwgrob@users.noreply.github.com>
2026-02-23 14:07:38 -0800

    Adding requirements and prototype outline
    Adding the first files that contains the requirements as well as the python file to make the prototype in

---
+++ protoype.py
@@ -0,0 +1,20 @@
+# Creating a sample python class to store different courses and reviews
+
+class course():
+    def __init__(self, CNO, Year):
+        pass
+
+    def createCourse(self):
+        pass
+
+class review(course):
+
+    def __init__(self):
+        pass
+
+    def createReview(self):
+        pass
+
+
+
+
