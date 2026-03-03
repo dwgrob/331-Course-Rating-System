5810d3221407364dc6c0b9fb48439c5900e20d00
Daniel Robinson <148491182+dwgrob@users.noreply.github.com>
2026-03-03 11:57:35 -0800

    Updating the courses
    Made it so that each course has its own list of reviews

--- prototype.py
+++ prototype.py
@@ -1,37 +1,50 @@
 # Creating a sample python class to store different courses and reviews
-   
+
+class review:
+    
+    def createReview(self):
+        self.difficulty = input("How difficult was the course on a scale from 1-5:")
+        self.workLoad = input("How was the workload on a scale from 1-5:")
+        self.enjoyment = input("How much did you enjoy the course on a scale from 1-5:")
+        self.comment = input("Any additional comments:")
 
 
-class review():
 
-    def __init__(self, courseNum, courseYear, difficulty, workLoad, enjoyment):
+class course():
+    def __init__(self, courseNum: int, program: str, courseYear: int):
         self.courseNum = courseNum
+        self.program = program
         self.courseYear = courseYear
-        self.difficulty = difficulty
-        self.workLoad = workLoad
-        self.enjoyment = enjoyment
+        self.reviewList = []
+    
+    def createReview(self):
+        print("For the course", self.courseNum, ":")
+        newReview = review()
+        newReview.createReview()
+        self.reviewList.append(newReview)
 
-    def view(self):
-        print("Course: ", self.courseNum)
-        print("Year: ", self.courseYear)
-        print("Difficulty: ", "*" * self.difficulty)
-        print("WorkLoad: ", "*" * self.workLoad)
-        print("Enjoyment: ", "*" * self.enjoyment)
-
-    def addComment(self):
-        self.comment = input("Enter your comment about the class: ")
-
-    def viewComment(self):
-        print(self.comment)
-
-
-
-r1 = review(265, 2, 2, 4, 5)
-r1.view()
-r1.addComment()
-r1.viewComment()
+    def listReviews(self):
+        print("The reviews for the course", self.courseNum, ":")
+        for review in self.reviewList:
+            print("Difficulty:", review.difficulty)
+            print("Enjoyment:", review.enjoyment)
+            print("Workload:", review.workLoad)
+            print("Comment:", review.comment)
 
 
 
 
 
+if __name__ == "__main__":
+    course1 = course(260, "CSCI", 2)
+    course2 = course(261, "CSCI", 2)
+    course3 = course(223, "MATH", 2)
+
+    course1.createReview()
+    course1.listReviews()
+    course1.createReview()
+    course2.createReview()
+    course3.createReview()
+    course1.listReviews()
+    course2.listReviews()
+    course3.listReviews()
