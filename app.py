131a607f2a0fc6f7ab04509fc06eb5bddbad56cd
Daniel Robinson <148491182+dwgrob@users.noreply.github.com>
2026-03-24 20:56:19 -0700

    generating reviews
    Created a function that generates reviews for each class, also created a way to delete all of the reviews for

--- app.py
+++ app.py
@@ -1,50 +1,9 @@
-<<<<<<< Updated upstream
-from flask import Flask, render_template
-
-class review:
-    
-    def createReview(self):
-        self.difficulty = input("How difficult was the course on a scale from 1-5:")
-        self.workLoad = input("How was the workload on a scale from 1-5:")
-        self.enjoyment = input("How much did you enjoy the course on a scale from 1-5:")
-        self.comment = input("Any additional comments:")
-
-
-
-class course():
-    def __init__(self, courseNum: int, program: str, courseYear: int):
-        self.courseNum = courseNum
-        self.program = program
-        self.courseYear = courseYear
-        self.reviewList = []
-    
-    def createReview(self):
-        print("For the course", self.courseNum, ":")
-        newReview = review()
-        newReview.createReview()
-        self.reviewList.append(newReview)
-
-    def listReviews(self):
-        print("The reviews for the course", self.courseNum, ":")
-        for review in self.reviewList:
-            print("Difficulty:", review.difficulty)
-            print("Enjoyment:", review.enjoyment)
-            print("Workload:", review.workLoad)
-            print("Comment:", review.comment)
-
-
-
-=======
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 import random
->>>>>>> Stashed changes
 
 app = Flask(__name__)
 
-<<<<<<< Updated upstream
-@app.route('/', methods = ['GET', 'POST'])
-=======
 db = SQLAlchemy(app)
 
 
@@ -130,16 +89,12 @@ def clearReviews():
     
 
 @app.route('/')
->>>>>>> Stashed changes
 def home():
     return render_template("home.html")
 
 
 @app.route('/createReview')
 def createReview():
-<<<<<<< Updated upstream
-    return 'test'
-=======
     courses = get_courses()
 
     if request.method == 'POST':
@@ -193,7 +148,8 @@ def delReviews():
     clearReviews()
     return redirect(url_for('home'))
 
->>>>>>> Stashed changes
 
 if __name__ == '__main__':
-    app.run()
+    with app.app_context():
+        seed_courses()
+    app.run(debug=True)
